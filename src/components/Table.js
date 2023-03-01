da113a55e37d6b0cb65d5104a2951abd4de9c8c7
Jackie1645 <jackieli412@gmail.com>
2023-03-01 12:43:48 -0500

    Updated candidate data tables

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,19 +2,22 @@ import React, { useRef, useEffect, useState } from "react";
 import flHouse22 from "../json/winners/flHouse2022.json";
 import gaHouse22 from "../json/winners/gaHouse2022.json";
 import paHouse22 from "../json/winners/paHouse2022.json";
+import flCandidates from "../json/candidates/FL2022_candidates.json";
+import gaCandidates from "../json/candidates/GA2022_candidates.json";
+import paCandidates from "../json/candidates/PA2022_candidates.json";
 
 const Table = ({ currentState }) => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     if (currentState === "florida") {
-      setData(flHouse22.results);
+      setData(flCandidates);
     }
     if (currentState === "georgia") {
-      setData(gaHouse22.results);
+      setData(gaCandidates);
     }
     if (currentState === "pennsylvania") {
-      setData(paHouse22.results);
+      setData(paCandidates);
     }
 
     if (currentState === null) {
@@ -26,18 +29,29 @@ const Table = ({ currentState }) => {
     <div className="incumbents">
       <table>
         <tr>
-          <th>District</th>
           <th>Name</th>
-          <th>Political Affiliation</th>
+          <th>Party</th>
           <th>Election Result</th>
+          <th>District</th>
+          <th>Political Affiliation</th>
+          <th>Incumbency?</th>
+          <th>Geographic Variation</th>
+          <th>Population Variation</th>
         </tr>
         {data.map((val, key) => {
+          // console.log(val);
+          let color = val.Party === 'DEM' ? 'blue' : 'red';
+          console.log(color);
           return (
             <tr key={key}>
-              <td>{val.district}</td>
-              <td>{val.name}</td>
-              <td>{val.party}</td>
-              <td>{"W"}</td>
+              <td>{val.Candidate}</td>
+              <td style={{color: color}}>{val.Party}</td>
+              <td>{val['Primary Outcome']}</td>
+              <td>{val.District}</td>
+              <td>{val.State}</td>
+              <td>{val.Incumbent}</td>
+              <td>{val['Primary Votes']}</td>
+              <td>{val['Primary %']}</td>
             </tr>
           );
         })}
