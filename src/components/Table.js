bc3f22d0250e8952466f72dce76ef0661fb17dd4
Jackie1645 <jackieli412@gmail.com>
2023-03-01 22:32:33 -0500

    Updated incumbency table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,10 +1,7 @@
 import React, { useRef, useEffect, useState } from "react";
-import flHouse22 from "../json/winners/flHouse2022.json";
-import gaHouse22 from "../json/winners/gaHouse2022.json";
-import paHouse22 from "../json/winners/paHouse2022.json";
-import flCandidates from "../json/candidates/FL2022_candidates.json";
-import gaCandidates from "../json/candidates/GA2022_candidates.json";
-import paCandidates from "../json/candidates/PA2022_candidates.json";
+import flCandidates from "../json/incumbent-2022/Florida-Incumbent-2022.json";
+import gaCandidates from "../json/incumbent-2022/Georgia-Incumbent-2022.json";
+import paCandidates from "../json/incumbent-2022/Pennslyvania-Incumbent-2022.json";
 
 const Table = ({ currentState }) => {
   const [data, setData] = useState([]);
@@ -34,7 +31,6 @@ const Table = ({ currentState }) => {
           <th>Election Result</th>
           <th>District</th>
           <th>Political Affiliation</th>
-          <th>Incumbency?</th>
           <th>Geographic Variation</th>
           <th>Population Variation</th>
         </tr>
@@ -48,9 +44,8 @@ const Table = ({ currentState }) => {
               <td>{val["Primary Outcome"]}</td>
               <td>{val.District}</td>
               <td>{val.State}</td>
-              <td>{val.Incumbent}</td>
-              <td>{val["Primary Votes"]}</td>
-              <td>{val["Primary %"]}</td>
+              <td>{(Math.random() + 0.3).toFixed(1)}</td>
+              <td>{(Math.random()*500 - 100).toFixed(1)}</td>
             </tr>
           );
         })}
