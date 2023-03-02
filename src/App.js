6b4dcd34f232dd153c37482d670a7c0e024d4f9d
Jackie1645 <jackieli412@gmail.com>
2023-03-02 04:17:26 -0500

    Added ensemble and open seats

--- src/App.js
+++ src/App.js
@@ -19,6 +19,24 @@ const App = () => {
 
   const [selectedPlan, setSelectedPlan] = useState(districtPlans[2022]);
 
+  let imag = "";
+  let seats = "";
+  let count = 0;
+  if (currentState === "florida") {
+    imag = "https://media.discordapp.net/attachments/1080353490171346954/1080779222857031751/florida.png";
+    seats = "https://cdn.discordapp.com/attachments/1080353490171346954/1080772506782269552/image.png";
+    count = 23;
+  }
+  if (currentState === "georgia") {
+    imag = "https://media.discordapp.net/attachments/1080353490171346954/1080779222617948160/georgia.png";
+    seats = "https://cdn.discordapp.com/attachments/1080353490171346954/1080774201578885200/image.png";
+    count = 13;
+  }
+  if (currentState === "pennsylvania") {
+    imag = "https://media.discordapp.net/attachments/1080353490171346954/1080779222425018409/pennsylvania.png";
+    seats = "https://cdn.discordapp.com/attachments/1080353490171346954/1080773577260937266/image.png";
+    count = 16;
+  }
   return (
     <div className="App">
       <Map
@@ -48,6 +66,15 @@ const App = () => {
             currentDistrict={currentDistrict}
           />
         )}
+        {currentState && (
+        <div className="ensemble"> 
+          <h3>Ensemble Information & Prediction</h3>
+          <strong>Number of District Plans: </strong> 10000 <br />
+          <strong>Number of Incumbents: </strong> {count} <br />
+          <strong>Number of Incumbents Predicted to Win: </strong> {count - 1} <br/>
+          <img src={imag} alt="Ensemble Box and Whiskers"/> <br/>
+          <img src={seats} alt="Open Seats"/> <br/>
+        </div>)}
       </div>
     </div>
   );
